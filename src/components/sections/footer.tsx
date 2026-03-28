e12fe9042d8351df359fc7559fc480a21c01527f
Syeda Khadeeja Al Zahra <s.khdeej@gmail.com>
2026-03-28 12:48:03 +0400

    Update footer.tsx

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -9,15 +9,15 @@ export function Footer() {
   }
 
   return (
-    <div className="flex max-md:flex-col justify-between items-center max-md:gap-2 mb-6 py-10 border-gray-800 border-t">
+    <div className="flex max-md:flex-col justify-between items-center max-md:gap-2 py-5 md:py-10 border-gray-800 border-t">
       <p className="text-muted-foreground text-base text-center" suppressHydrationWarning>
-        Syeda Khadeeja · {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()} · Syeda Khadeeja
       </p>
       <button
         className="flex items-center gap-2 w-fit text-muted-foreground text-base cursor-pointer"
         onClick={scrollToTop}
       >
-        Elevate to the top
+        Back to top
         <ArrowUp size={20} />
       </button>
     </div>
